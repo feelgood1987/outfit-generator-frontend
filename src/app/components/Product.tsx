c92bb3c28498ac664f3b9c7d3305414261f54813
feelgood1987 <feelgood1987@gmail.com>
2024-10-12 14:28:04 +0300

    feat(frontend): next.js project initialization

---
+++ src/app/components/Product.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+const Product = () => {
+  return (
+    <Container>
+      <Typography variant="h1" component="h1">
+        Product
+      </Typography>
+    </Container>
+  );
+};
+
+export default Product;
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import Product from "./components/Product";
+
+export default function Home() {
+  return <Product />;
+}
